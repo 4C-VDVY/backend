9ea390da37e2f88fb1acd6197b520f7c2df0928e
Vedant Kulkarni <vedantkulkarni100@gmail.com>
2023-04-14 16:11:20 +0530

    added gitignore and env files

--- digital_marketer/settings.py
+++ digital_marketer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x_r(zl0p=1-t$qvetm4!3vwpar&b+e#1fkvqbk&4_w^o^=h)fw'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'digital',
         'USER': 'root',
-        'PASSWORD': 'VedKulk@100',
+        'PASSWORD': config('password'),
         'HOST':'localhost',
         'PORT':'3306',
     }
